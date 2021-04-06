d1ba32889f656e5d36194b3a3c554aa9a4efc411
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2021-04-05 21:53:00 -0400

    Add project files.

---
+++ src/SmartAdmin.WebUI/ViewComponents/NavigationViewComponent.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SmartAdmin.WebUI.Models;
+
+namespace SmartAdmin.WebUI.ViewComponents
+{
+    public class NavigationViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var items = NavigationModel.Seed;
+
+            return View(items);
+        }
+    }
+}
