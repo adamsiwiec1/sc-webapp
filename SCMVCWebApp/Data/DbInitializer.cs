ef896001996ccd44b505a6ab1d10183f5dc72e72
adamsiwiec1 <73085927+adamsiwiec1@users.noreply.github.com>
2021-04-10 00:29:34 -0400

    stuooopid references

--- SCMVCWebApp/Data/DbInitializer.cs
+++ SCMVCWebApp/Data/DbInitializer.cs
@@ -15,34 +15,34 @@ namespace SCMVCWebApp.Data
         public static void Initialize(IServiceProvider services)
         {
 
-            //#1 -- database service
-            ApplicationDbContext database = services.GetRequiredService<ApplicationDbContext>();
+            ////#1 -- database service
+            //ApplicationDbContext database = services.GetRequiredService<ApplicationDbContext>();
 
-            //#2 -- role service
-            //startup.cs line 35 add --> .AddRoles<IdentityRole>()  (this is different from what we do next step, keep this)
-            RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            ////#2 -- role service
+            ////startup.cs line 35 add --> .AddRoles<IdentityRole>()  (this is different from what we do next step, keep this)
+            //RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
 
-            //#3 -- user service 
-            //startup.cs line 34 change --> to ApplicationUser from IdentityUser (not where we added it in the prior step, but one line above)
-            //_LoginPartial.cshtml change --> line 2 & 3 to ApplicationUser from IdentityUser
-            UserManager<ApplicationUser> userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            ////#3 -- user service 
+            ////startup.cs line 34 change --> to ApplicationUser from IdentityUser (not where we added it in the prior step, but one line above)
+            ////_LoginPartial.cshtml change --> line 2 & 3 to ApplicationUser from IdentityUser
+            //UserManager<ApplicationUser> userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
 
-            string studentRole = "Student";
-            string tutorRole = "Tutor";
-            string administratorRole = "Administrator";
+            //string studentRole = "Student";
+            //string tutorRole = "Tutor";
+            //string administratorRole = "Administrator";
 
-            if (!database.Roles.Any())
-            {
-                IdentityRole role = new IdentityRole(studentRole);
-                roleManager.CreateAsync(role).Wait();
-                //role manager creates AND saves, unlike db/user manager
+            //if (!database.Roles.Any())
+            //{
+            //    IdentityRole role = new IdentityRole(studentRole);
+            //    roleManager.CreateAsync(role).Wait();
+            //    //role manager creates AND saves, unlike db/user manager
 
-                role = new IdentityRole(tutorRole);
-                roleManager.CreateAsync(role).Wait();
+            //    role = new IdentityRole(tutorRole);
+            //    roleManager.CreateAsync(role).Wait();
 
-                role = new IdentityRole(administratorRole);
-                roleManager.CreateAsync(role).Wait();
-            }
+            //    role = new IdentityRole(administratorRole);
+            //    roleManager.CreateAsync(role).Wait();
+            //}
         }
     }
 }
--- SCMVCWebApp/Startup.cs
+++ SCMVCWebApp/Startup.cs
@@ -1,18 +1,91 @@
+//using Microsoft.AspNetCore.Builder;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.AspNetCore.HttpsPolicy;
+//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.UI;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.Hosting;
+//using SCMVCWebApp.Data;
+//using SCMVCWebApp.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace SCMVCWebApp
+//{
+//    public class Startup
+//    {
+//        public Startup(IConfiguration configuration)
+//        {
+//            Configuration = configuration;
+//        }
+
+//        public IConfiguration Configuration { get; }
+
+//        // This method gets called by the runtime. Use this method to add services to the container.
+//        public void ConfigureServices(IServiceCollection services)
+//        {
+//            services.AddDbContext<ApplicationDbContext>(options =>
+//                options.UseSqlServer(
+//                    Configuration.GetConnectionString("DefaultConnection")));
+//            //services.AddDatabaseDeveloperPageExceptionFilter();
+
+//            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//                .AddEntityFrameworkStores<ApplicationDbContext>();
+//            services.AddControllersWithViews();
+//        }
+
+//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+//        {
+//            if (env.IsDevelopment())
+//            {
+//                app.UseDeveloperExceptionPage();
+//                app.UseMigrationsEndPoint();
+//            }
+//            else
+//            {
+//                app.UseExceptionHandler("/Home/Error");
+//                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//                app.UseHsts();
+//            }
+//            app.UseHttpsRedirection();
+//            app.UseStaticFiles();
+
+//            app.UseRouting();
+
+//            app.UseAuthentication();
+//            app.UseAuthorization();
+
+//            app.UseEndpoints(endpoints =>
+//            {
+//                endpoints.MapControllerRoute(
+//                    name: "default",
+//                    pattern: "{controller=AspNetCore}/{action=Welcome}/{id?}");
+//                endpoints.MapRazorPages();
+//            });
+//        }
+//    }
+//}
+
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using SCMVCWebApp.Data;
 using SCMVCWebApp.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SCMVCWebApp
 {
@@ -28,14 +101,39 @@ namespace SCMVCWebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<SmartSettings>(Configuration.GetSection(SmartSettings.SectionName));
+
+            // Note: This line is for demonstration purposes only, I would not recommend using this as a shorthand approach for accessing settings
+            // While having to type '.Value' everywhere is driving me nuts (>_<), using this method means reloaded appSettings.json from disk will not work
+            services.AddSingleton(s => s.GetRequiredService<IOptions<SmartSettings>>().Value);
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
+                .AddRoleManager<RoleManager<IdentityRole>>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            services.AddControllersWithViews();
+
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services
+                .AddControllersWithViews();
+
+            services.AddRazorPages();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.LoginPath = "/Identity/Account/Login";
+                options.LogoutPath = "/Identity/Account/Logout";
+                options.AccessDeniedPath = "/Identity/Account/AccessDenied";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +142,6 @@ namespace SCMVCWebApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
             }
             else
             {
@@ -52,19 +149,18 @@ namespace SCMVCWebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=AspNetCore}/{action=Welcome}/{id?}");
+                    "default",
+                    "{controller=AspNetCore}/{action=Welcome}");
                 endpoints.MapRazorPages();
             });
         }
